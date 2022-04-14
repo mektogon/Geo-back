b3f66a95be05aaa46d849c8e0ce6f91f8d95da66
Maxim Dorofeev <Mektogon385@mail.ru>
2022-04-14 19:45:19 +0700

    Начало разработки.

---
+++ src/main/java/ru/dorofeev/mobilemap/MobileMapApplication.java
@@ -0,0 +1,13 @@
+package ru.dorofeev.mobilemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/dorofeev/mobilemap/MobileMapApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dorofeev.mobilemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
